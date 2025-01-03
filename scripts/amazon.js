f848121b118f49b9533d9f55655f8fea2f26c72b
oladipupot <oladipupotoheeb75@gmail.com>
2025-01-03 21:03:54 +0100

    use modules and import out from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-
+import { cart } from "../data/cart.js";
 
 let productsHTML =  '';
 
@@ -87,6 +87,6 @@ document.querySelectorAll('.js-add-to-cart').forEach ((button) => {
         });
 
         document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-
+ 
     }); 
 });
